32530b3ff7c795fe6879d64429b0d0b7770d3680
PritamIT2023 <pritammondal8981@email.com>
2024-01-08 10:54:00 +0530

    feat: Add Models
    CommunicationTemplates MailComms SmsComms WhatsAppComms Otps Models added
    
    Ref: #3

--- service/models.registry.js
+++ service/models.registry.js
@@ -1,9 +1,35 @@
 const CommunicationHistories = require("./models/CommunicationHistory.model");
+const CommunicationTemplates = require("./models/CommunicationTemplates.model");
+const MailComms = require("./models/MailComms.model");
+const Otps = require("./models/Otp.model");
+const SmsComms = require("./models/SmsComms.model");
+const WhatsAppComms = require("./models/WhatsAppComms.model");
+
 const modelsRegistry = {
-    CommunicationHistories: {
-        database: "application",
-        model: CommunicationHistories,
-      },
+  CommunicationHistories: {
+    database: "application",
+    model: CommunicationHistories,
+  },
+  CommunicationTemplates: {
+    database: "application",
+    model: CommunicationTemplates,
+  },
+  MailComms: {
+    database: "application",
+    model: MailComms,
+  },
+  Otps: {
+    database: "application",
+    model: Otps,
+  },
+  SmsComms: {
+    database: "application",
+    model: SmsComms,
+  },
+  WhatsAppComms: {
+    database: "application",
+    model: WhatsAppComms,
+  },
 };
 
 exports.modelsRegistry = modelsRegistry;
