7d94041d9dc91278b522a60c4482a32911e92f4b
PritamIT2023 <pritammondal8981@email.com>
2024-05-21 14:54:17 +0530

    fix(global): :bug: remove all models from models registry
    remove all models from models registry and fix model json files
    
    Ref: #25

--- service/models.registry.ts
+++ service/models.registry.ts
@@ -1,35 +1,3 @@
-import { CommunicationHistories } from "./models/CommunicationHistory.model";
-import { CommunicationTemplates } from "./models/CommunicationTemplates.model";
-import { MailComms } from "./models/MailComms.model";
-import { Otps } from "./models/Otp.model";
-import { SmsComms } from "./models/SmsComms.model";
-import { WhatsAppComms } from "./models/WhatsAppComms.model";
-
-const ModelsRegistry = {
-  CommunicationHistories: {
-    database: "application",
-    model: CommunicationHistories,
-  },
-  CommunicationTemplates: {
-    database: "application",
-    model: CommunicationTemplates,
-  },
-  MailComms: {
-    database: "application",
-    model: MailComms,
-  },
-  Otps: {
-    database: "application",
-    model: Otps,
-  },
-  SmsComms: {
-    database: "application",
-    model: SmsComms,
-  },
-  WhatsAppComms: {
-    database: "application",
-    model: WhatsAppComms,
-  },
-};
+const ModelsRegistry = {};
 
 export default ModelsRegistry;
